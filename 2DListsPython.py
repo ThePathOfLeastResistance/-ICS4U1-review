cb5e4620ac080d66ea87e1551dd0d8480a71f041
James Cao <jameslodgemath@gmail.com>
2025-09-22 12:33:07 -0400

    second problem

--- 2DListsPython.py
+++ 2DListsPython.py
@@ -10,6 +10,8 @@ for i in range(0, rows):
         rowValue.append(int(input("Enter the number")))
     table.append(rowValue)
     
+'''
+#1 
 #Create a user defined 2D list and sum a row of their choice
 
 rowRequest = int(input("Which Row do you want the sum of?: "))
@@ -21,4 +23,18 @@ print(table[rowRequest-1])
 
 for i in table[rowRequest-1]:
     sum += i
-print(f'The sum of all the values in that row is {sum}')
+print(f'The sum of all the values in that row is {sum}')'''
+
+#2
+#Create a user defined 2D list and sum a column of their choice
+
+# the negative one is to compensate for humans counting at 1 and computers starting a 0 
+
+columnRequest = int(input("What Column do you want the sum of? ")) - 1
+
+sum = 0 
+for i in range(0, len(table)):
+    if len(table[i]) >= columnRequest:
+        sum += table[i][columnRequest]
+            
+print(f'The sum of all the column in that row is {sum}')
