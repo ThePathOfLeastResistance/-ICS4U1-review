b2b1d0b158d4cc020e754ef708d53bde884366e1
James Cao <jamescao800@gmail.com>
2025-09-30 12:03:35 -0400

    redid the printing function removing the complex zip
    Signed-off-by: James Cao <jamescao800@gmail.com>

--- GameOfLife.py
+++ GameOfLife.py
@@ -7,31 +7,48 @@ import random
 
 
 
+# # Functions are here
+# def print_board(board, columnValue):
+#     os.system('cls' if os.name == 'nt' else 'clear')
+#     listOfLifePrints = []
+#     itemCounter = 0
+#     for row in board:
+#         for lifeState in row: 
+#             itemCounter += 1
+#             # print(itemCounter)
+#             # print(itemCounter % 3 == 0)
+#             if lifeState == 1:
+#                 listOfLifePrints.append(["000", "000"] )
+#             else:
+#                 listOfLifePrints.append(["[ ]", "[ ]"] )
+#         #the * sign unpacks the zip
+#         # for the list of list, it unpacks the inner list as seperate arguments
+#     newList = ["   ".join(rowOfText) for rowOfText in zip(*listOfLifePrints)]
+#     newZipList = list(zip(*listOfLifePrints))
+#     print("\n")
+#     for indexValue in range(0, int(len(listOfLifePrints)/columnValue)):
+#         print(*newZipList[0][indexValue * columnValue : (indexValue + 1)*columnValue])
+#         print(*newZipList[1][indexValue * columnValue : (indexValue + 1)*columnValue])        
+#         print('\n')
+#     print(board)
+
 # Functions are here
 def print_board(board, columnValue):
     os.system('cls' if os.name == 'nt' else 'clear')
     listOfLifePrints = []
-    itemCounter = 0
     for row in board:
         for lifeState in row: 
-            itemCounter += 1
-            # print(itemCounter)
-            # print(itemCounter % 3 == 0)
             if lifeState == 1:
-                listOfLifePrints.append(["000", "000"] )
+                listOfLifePrints.append("0000" )
             else:
-                listOfLifePrints.append(["[ ]", "[ ]"] )
-        #the * sign unpacks the zip
-        # for the list of list, it unpacks the inner list as seperate arguments
-    newList = ["   ".join(rowOfText) for rowOfText in zip(*listOfLifePrints)]
-    newZipList = list(zip(*listOfLifePrints))
-    print("\n")
+                listOfLifePrints.append("[  ]")
+        
     for indexValue in range(0, int(len(listOfLifePrints)/columnValue)):
-        print(*newZipList[0][indexValue * columnValue : (indexValue + 1)*columnValue])
-        print(*newZipList[1][indexValue * columnValue : (indexValue + 1)*columnValue])        
+        print("  ".join(listOfLifePrints[indexValue * columnValue : (indexValue + 1)*columnValue]))
+        print("  ".join(listOfLifePrints[indexValue * columnValue : (indexValue + 1)*columnValue]))
+
         print('\n')
     print(board)
-    
     
 def populate_board(rowValue, columnValue):
     newBoard = []
