7b87a2ff6fbc95c39c71b4a3ebe6974674a3aad6
James Cao <jamescao800@gmail.com>
2025-10-05 13:57:34 -0400

    cleaned up code base by removing print statments
    Signed-off-by: James Cao <jamescao800@gmail.com>

--- GameOfLife.py
+++ GameOfLife.py
@@ -3,36 +3,10 @@
 # The game of life simulation in Python 
 
 
-#test tes ttes
 
 import os
 import random
 import time 
-# # Functions are here
-# def print_board(board, columnValue):
-#     os.system('cls' if os.name == 'nt' else 'clear')
-#     listOfLifePrints = []
-#     itemCounter = 0
-#     for row in board:
-#         for lifeState in row: 
-#             itemCounter += 1
-#             # print(itemCounter)
-#             # print(itemCounter % 3 == 0)
-#             if lifeState == 1:
-#                 listOfLifePrints.append(["000", "000"] )
-#             else:
-#                 listOfLifePrints.append(["[ ]", "[ ]"] )
-#         #the * sign unpacks the zip
-#         # for the list of list, it unpacks the inner list as seperate arguments
-#     newList = ["   ".join(rowOfText) for rowOfText in zip(*listOfLifePrints)]
-#     newZipList = list(zip(*listOfLifePrints))
-#     print("\n")
-#     for indexValue in range(0, int(len(listOfLifePrints)/columnValue)):
-#         print(*newZipList[0][indexValue * columnValue : (indexValue + 1)*columnValue])
-#         print(*newZipList[1][indexValue * columnValue : (indexValue + 1)*columnValue])        
-#         print('\n')
-#     print(board)
-
 class GridClass:
     def __init__(self):
         self.gridOfCells = [[], [],[]]
@@ -66,21 +40,13 @@ class GridClass:
             for column in range(0, userColumnValue):
                 newColumn.append(random.randint(0,1))
             newBoard.append(newColumn)
-        # print("the NEW BOARD")
-        # print(newBoard)
         self.gridOfCells = newBoard
-        # print("the New grid of Cells")
-        # print(self.gridOfCells)
 
     def NextGeneration(self, boardIndex):
         for rowIndex in range(0, len(self.gridOfCells)):
             for columnIndex in range(0, len(self.gridOfCells[0])):
                 cell = Cell()
-                # print("the current grid of cells")
-                # print(self.gridOfCells)
                 self.gridOfCells[rowIndex][columnIndex] = 1 if cell.CheckNeighbours(boardIndex, rowIndex, columnIndex) else 0
-                # print("the NEW grid of cells")
-                # print(self.gridOfCells)
 
                     
 class Cell:
@@ -96,39 +62,24 @@ class Cell:
                 referenceXIndex = columnIndex + xIndex
                
                 if 0 <= referenceXIndex < len(boardIndex[0]) and 0<= referenceYIndex < len(boardIndex) and not (yIndex == 0 and xIndex == 0):
-                    print('pass NUmber ONe')
-                    print("ReferenceYIndex")
-                    print(referenceYIndex)
-                    print("ReferenceXIndex")
-                    print(referenceXIndex)
-                    print(yIndex, xIndex)
-                    print(rowIndex, columnIndex)
-                    print(boardIndex)
                     if boardIndex[referenceYIndex][referenceXIndex] == 1:
                         print('added Neighbour')
                         neighbourCount += 1
 
-        print("neighbourCOUnt")    
-        print(neighbourCount)
         if boardIndex[rowIndex][columnIndex] == 1:
             if neighbourCount < 2:
-                print("false 1")
                 return False
             elif neighbourCount == 2 or neighbourCount == 3: 
-                print("True 2") 
                 return True
                 
             elif neighbourCount > 3:
-                print('False 3 ') 
                 return False
             else:
                 print("Somthing is not right!!")
         elif boardIndex[rowIndex][columnIndex] == 0:
             if neighbourCount == 3:
-                print("true 4") 
                 return True
             else:
-                print("false 5") 
                 return False
             
             
