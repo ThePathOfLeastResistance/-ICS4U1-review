564c88f37a06d06501ea6fb45dac15d214ac9f87
James Cao <jamescao800@gmail.com>
2025-11-13 13:15:43 -0500

    finished everything

--- Recursion.py
+++ Recursion.py
@@ -45,9 +45,49 @@ def string_Partition(inputString):
 #Given a string digits containing only digits (0-9) and an integer target, find all possible expressions that evaluate to the target value using the binary operators +, -, and *. If no such expression is possible, return an empty list (10 marks).
 #skip 
 
+listOfOperation = ["*", "+", "-", "C"]
 
+def recurisonCal(listIn, operationQue, target):
+    if len(listIn) == 0:
+        print('before')
+        print(operationQue)
+        if eval(" ".join(operationQue)) == target:
+            print(operationQue)
+        return
+            
+    for i in listOfOperation:
+        if i == "C":
+            recurisonCal(listIn[1:], operationQue[:-2] + [f"{operationQue[-2]}{listIn[0]}"], target)
+        elif len(listIn) == 1:
+            recurisonCal(listIn[1:], operationQue + [str(listIn[0])], target)
+        else:
+            recurisonCal(listIn[1:], operationQue + [str(listIn[0])] + [i], target)
+    return recurisonCal
+
+# recurisonCal([1,2,9], [],7 )
+
+#that was the first try, did not work out well
+
+def recFunc(listIn, target):
+    def _backtrack(index, exp):
+        if index == len(listIn):
+            if eval(exp) == target:
+                print(exp)
+        
+            return
+        digit = str(listIn[index])  
+        
+        if index == 0:
+            _backtrack(index + 1, digit)
+        else: 
+            _backtrack(index + 1, exp + "+" + digit)
+            _backtrack(index + 1, exp + "-" + digit)
+            _backtrack(index + 1, exp + "*" + digit)
+            _backtrack(index + 1, exp + digit) 
+    _backtrack(0, "")
 #Given an array of integers, print a sum triangle from it such that the first level has all array elements. From then, at each level number of elements is one less than the previous level and elements at the level is be the Sum of consecutive two elements in the previous level (8 marks). 
 
+recFunc([1,2,4], 7)
 
 
 listExample = [1,2,3,4,5]
@@ -63,9 +103,9 @@ def levels(listOut, index):
     listOut = arrayList
     return levels(listOut,  index)
     
-for index in range(1, len(listExample)+1):
-    newList = levels(listExample, index)
-    print(newList)
+# for index in range(1, len(listExample)+1):
+#     newList = levels(listExample, index)
+#     print(newList)
              
 #You are given an array arr[] consisting of n elements. Your task is to generate and print all possible combinations of exactly r elements from this array.
 
@@ -80,3 +120,17 @@ for index in range(1, len(listExample)+1):
 #     return N_Element(arr, r)
 # N_Element([1,2,3,4, 5,6], 4)
 
+#I realized this way was very inefficent, I asked gpt to see how they would do it as it would be more concise and the way that I choose had limiations and I ran into issues that I do not think I could fix. A
+def N_Element(arr, r, current ):
+    if r == 0:
+        print(current)
+        return 
+    if len(arr) == 0:
+        # this means that you ran out of stuff in the list to pick from
+        return
+    
+    N_Element(arr[1:], r - 1, current + [arr[0]])
+    
+    N_Element(arr[1:], r, current)
+    
+#N_Element([1,2,3,4,5,6], 3, [])
